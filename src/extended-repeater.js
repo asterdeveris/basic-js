63ec83e5dd5289eeb323d34cf4d57ab0fb19eade
Dasha <asterdeveris@gmail.com>
2023-04-16 23:01:10 +0300

    first commit

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,59 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let {
+    repeatTimes,
+    separator = "+",
+    addition,
+    additionRepeatTimes,
+    additionSeparator = "|",
+  } = options;
+
+  let additionString;
+  let result = String(str);
+
+  if (typeof addition !== "undefined") {
+    additionString = String(addition);
+
+    if (additionRepeatTimes) {
+      let additionArr = [];
+
+      for (let i = 0; i < additionRepeatTimes; i++) {
+        additionArr.push(additionString);
+      }
+      additionString = additionArr.join(`${additionSeparator}`);
+    }
+
+    result += additionString;
+  }
+
+  if (repeatTimes) {
+    let resultArr = [];
+
+    for (let i = 0; i < repeatTimes; i++) {
+      resultArr.push(result);
+    }
+    result = resultArr.join(`${separator}`);
+  }
+
+  return result;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,71 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }
+
+  const controlSequences = arr.reduce((arr, el, i) => {
+    if (typeof el === "string" && el.startsWith("--")) {
+      arr.push([el, i]);
+    }
+    return arr;
+  }, []);
+
+  let result = [...arr];
+
+  controlSequences.forEach((element) => {
+    const [sequence, index] = element;
+
+    if (sequence === "--double-next") {
+      if (arr[index + 1]) {
+        result.splice(index, 1, arr[index + 1]);
+      } else {
+        result.splice(index, 1);
+      }
+    }
+
+    if (sequence === "--discard-prev") {
+      if (index - 1 > 0) {
+        result.splice(index - 1, 2);
+      } else {
+        result.splice(index, 1);
+      }
+    }
+
+    if (sequence === "--discard-next") {
+      if (index !== arr.length - 1) {
+        result.splice(index, 2);
+      } else {
+        result.splice(index, 1);
+      }
+    }
+
+    if (sequence === "--double-prev") {
+      if (index - 1 > 0) {
+        result.splice(index, 1, arr[index - 1]);
+      } else {
+        result.splice(index, 1);
+      }
+    }
+  });
+
+  return result;
 }
 
 module.exports = {
-  transform
+  transform,
 };
